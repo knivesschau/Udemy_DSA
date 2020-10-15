a96fb70af0e6a291be8d886674f1e4e27353c87a
Catrina Fuentes <cat.fuentes93@gmail.com>
2020-10-14 18:55:03 -0700

    solved maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,29 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+// first attempt 
+function maxChar(str) {
+     const letters = {};
+     let counter = 0;
+     let recurChara = '';
+
+     for (let chara of str) { // for-of used to iterate over strings and arrays. 
+         if (letters[chara]) {
+             letters[chara]++;
+         }
+         else {
+             letters[chara] = 1; 
+         }
+     }
+
+     for (let chara in letters) { // for in used to iterate over objects.
+         if (letters[chara] > counter) {
+             counter = letters[chara];
+             recurChara = chara; 
+         }
+     }
+
+     return recurChara;
+}
 
 module.exports = maxChar;
